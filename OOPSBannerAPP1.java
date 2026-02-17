413190421558390f13198e841a0b86977f80c4a4
Riddhim Gupta <riddhimgupta2708@gmail.com>
2026-02-17 08:49:52 +0530

    Create OOPSBannerAPP1.java

---
+++ OOPSBannerAPP1.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp
+ * UC1: Print OOPS to Console
+ * @author Riddhim
+ * @version 2.0
+ */
+
+public class OOPSBannerAPP1 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   ******   ******* ");
+        System.out.println(" **  **  **  **  **  **     ****  ");
+        System.out.println(" **  **  **  **  **  **     **    ");
+        System.out.println(" **  **  **  **  ******   ******* ");
+        System.out.println(" **  **  **  **  **            ** ");
+        System.out.println(" **  **  **  **  **        **  ** ");
+        System.out.println(" *****   *****   **      ******* ");
+
+    }
+}
